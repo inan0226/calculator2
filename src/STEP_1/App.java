5bed4e743dad9aeba16d2aa2b4dd06af5bcab556
rladl <rladlsdks12@gmail.com>
2026-03-19 11:58:39 +0900

    STEP_1 ing

--- src/STEP_1/App.java
+++ src/STEP_1/App.java
@@ -1,4 +1,18 @@
 package STEP_1;
 
+import java.util.Scanner;
+
 public class App {
-}
+
+        public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
+    }
+
+
+    }
